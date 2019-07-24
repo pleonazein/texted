ecdfb3adceb00a265ac1bebce0229f6b32fb8cdf
zzalpha <neinzztein@gmail.com>
2019-07-24 17:55:00 +0530

    loop reading a single character from STDIN, and doing nothing else.

--- kilo.c
+++ kilo.c
@@ -2,6 +2,11 @@
 
    The development of this program is across 184 steps; which I shall track using git, and push to my repo "texted" */
 
+#include <unistd.h>
+
+
 int main() {
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1);
   return 0;
 }
