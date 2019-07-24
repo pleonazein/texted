7faed0804825b0159c9e640313e6ed254b310a26
zzalpha <neinzztein@gmail.com>
2019-07-24 15:41:14 +0530

    First commit.
    kilo.c contains the code for the text editor; will update the code with each step.

---
+++ kilo.c
@@ -0,0 +1,7 @@
+/* this project will be following the snaptoken tutorial on writing your own text editor, in C, amounting to around 1000 LOC.
+
+   The development of this program is across 184 steps; which I shall track using git, and push to my repo "texted" */
+
+int main() {
+  return 0;
+}
