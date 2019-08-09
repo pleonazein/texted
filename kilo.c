d7d0446afe228bc12f04e2a981139bba9d0caed0
zzalpha <neinzztein@gmail.com>
2019-08-09 14:36:18 +0530

    read output of printing DSR to terminal; parse output of GetCursorPosition using sscanf; fix missing tilde on last line

--- kilo.c
+++ kilo.c
@@ -119,8 +119,13 @@ void editorRefreshScreen() {
 }
 
 void editorDrawRows() {
-  for(int y=0; y < 48; y++)
-    write(STDOUT_FILENO, "~\r\n",3);
+  
+  for(int y=0; y < E.screenrows; y++){
+    write(STDOUT_FILENO, "~",3);
+
+  if(y < E.screenrows - 1) 
+    write(STDOUT_FILENO,"\r\n",2);
+  }
 }
 
 void clearScreen() {
@@ -133,7 +138,7 @@ int getWindowSize(int *rows, int *cols) {
 
   struct winsize ws;
 
-  if(1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+  if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B",12) != 12)
       return -1;
     return getCursorPosition(rows, cols);
@@ -153,21 +158,26 @@ void initEditor() {
 
 int getCursorPosition (int *rows, int *cols) {
 
+
+  char buf[32];
+  unsigned int i = 0;
+  
   if(write(STDOUT_FILENO, "\x1b[6n",4) != 4)
     return -1;
 
-  printf("\r\n");
-  char c;
-
-  while(read(STDIN_FILENO, &c, 1) == 1) {
-    if(iscntrl(c)) {
-      printf("%d\r\n",c);
-    } else {
-      printf("%d ('%c')\r\n",c,c);
-    }
+  while(i< sizeof(buf)-1) {
+    if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if(buf[i] == 'R') break;
+    i++;
   }
+  buf[i] = '\0';
 
-  editorReadKey();
+  printf("\r\n&buf[1]: '%s'\r\n",&buf[1]);
+
+  if(buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if(sscanf((buf+2), "%d;%d", rows, cols) != 2)
+    return -1;
 
   return 0;
 }
